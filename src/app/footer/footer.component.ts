53df279c18db4656bb41a32531bbae39dd8e9b60
lucagiov_work <92460366+lucagiov93@users.noreply.github.com>
2024-10-05 12:24:40 +0200

    Footer setting
    Aggiunti gli elementi di footer, e pulito app.component.html

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -5,6 +5,16 @@ import { Component } from '@angular/core';
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
+
 export class FooterComponent {
+
+  isVisible_1=true;
+  isVisible_2=true;
+  isVisible_3=true;
+
+
+  text_footer_1 ="Questo è il testo del div 1"
+  text_footer_2 ="Questo è il testo del div 2"
+  text_footer_3 ="Questo è il testo del div 3"
 
 }
